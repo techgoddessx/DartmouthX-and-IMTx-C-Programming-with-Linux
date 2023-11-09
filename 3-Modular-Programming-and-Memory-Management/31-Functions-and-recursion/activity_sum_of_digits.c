dffefa85d6c8984aa53b9d6a81bf8ee6cac321bf
goddess <secretgoddess6666@gmail.com>
2023-11-10 00:02:12 +0100

    update code

--- 3-Modular-Programming-and-Memory-Management/31-Functions-and-recursion/activity_sum_of_digits.c
+++ 3-Modular-Programming-and-Memory-Management/31-Functions-and-recursion/activity_sum_of_digits.c
@@ -6,14 +6,20 @@ int main(void)
 {
 	int n;
 	scanf("%d",&n);
+	
+	int total = 0;
 
 	while (n > 0)
 	{
 		int mod = n % 10; // split last digit from number
 		printf("%d\n",mod); // print the digit
+		
+		total += mod;
 
 		n = n / 10; // divide n by 10.
 	}
+
+	printf("the total is : %d.\n",total);
 
 	return 0;
 }
