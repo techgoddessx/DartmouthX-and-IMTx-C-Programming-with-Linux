cc59fdc7cabc8b1f2909b512f74c616daa56fbc9
goddess <146351038+techgoddessx@users.noreply.github.com>
2023-11-26 14:05:12 +0100

    Update strings-in-pointers.c

--- 4-Pointers-and-Memory-Management/42-multidimensional-arrays/strings-in-pointers.c
+++ 4-Pointers-and-Memory-Management/42-multidimensional-arrays/strings-in-pointers.c
@@ -19,17 +19,17 @@ int main(void){
 	char * spointer[] = {array1,array2};
 	printf("%s %s\n",spointer[0],spointer[1]);
 	// ---------------------------------- 
-    char a[4];
-    char b[6];
-    char c[9];
-    char * words[3] = {a, b, c};
-    printf("Please enter a word with at  most 3 letters: ");
-    scanf("%s", a);
-    printf("Please enter a word with at  most 5 letters: ");
-    scanf("%s", b);
-    printf("Please enter a word with at  most 8 letters: ");
-    scanf("%s", c);
-    printf("You entered: \n");
-    printf("%s %s %s.\n", a, b, c);
-    printf("%s %s %s.\n", words[0], words[1], words[2]);
+        char a[4];
+        char b[6];
+	char c[9];
+        char * words[3] = {a, b, c};
+        printf("Please enter a word with at  most 3 letters: ");
+        scanf("%s", a);
+        printf("Please enter a word with at  most 5 letters: ");
+        scanf("%s", b);
+        printf("Please enter a word with at  most 8 letters: ");
+        scanf("%s", c);
+	printf("You entered: \n");
+	printf("%s %s %s.\n", a, b, c);
+	printf("%s %s %s.\n", words[0], words[1], words[2]);
 }
