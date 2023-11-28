3c667939da9a8652dd6a999c5b4672822b9e8a67
goddess <secretgoddess6666@gmail.com>
2023-11-28 23:20:37 +0100

    new file

--- 5-Advanced-Data-Types/52-Structures-and-pointers/newfile.c
+++ 5-Advanced-Data-Types/52-Structures-and-pointers/newfile.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-struct point{
+struct point {
 	int x;
 	int y;
+	struct point * next;
 };
-void reads(struct point *);
-void printds(struct point *,int );
+
+void printlinkedlist(struct point *);
+
 int main(void){
-	struct point * poly;
-	int vertices;
-	printf("how many vertices? \n");
-	scanf("%d",&vertices);
-	poly = (struct point *) malloc(vertices*sizeof(struct point));
-	for (int i = 0; i < vertices; i++){
-		reads(&poly[i]);
-	}
-	printds(&poly[0],vertices);
+	struct point p1 = {1,2,NULL};
+	struct point p2 = {3,4,NULL};
+	struct point p3 = {5,6,NULL};
+	struct point p4 = {7,8,NULL};
+
+	struct point *start;
+
+	start = &p1;
+	p1.next = &p2;
+	p2.next = &p3;
+
+	printlinkedlist(start);
+	
 	return 0;
 }
-void reads(struct point *ptr){
-	printf("x: ");
-	scanf("%d",&ptr->x);
-	printf("y: ");
-        scanf("%d",&ptr->y);
-}
-void printds(struct point *ptr,int vertices){
-	for(int i = 0; i < vertices; i++){
-		printf("%d,%d\n",ptr[i].x,ptr[i].y);
+
+void printlinkedlist(struct point * ptr1){
+	while(ptr1 != NULL){
+		printf("%d %d\n",ptr1->x,ptr1->y);
+		ptr1 = ptr1->next;
 	}
 }
 
