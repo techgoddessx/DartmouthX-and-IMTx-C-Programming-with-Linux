3c6f99436ba1950d6959d7532755c2788e9a87a5
goddess <secretgoddess6666@gmail.com>
2023-12-01 23:09:12 +0100

    update

--- 5-Advanced-Data-Types/53-Linked-lists/practice.c
+++ 5-Advanced-Data-Types/53-Linked-lists/practice.c
@@ -1,39 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 struct digit {
-	int dig;
+	int n;
 	struct digit * next;
 };
-struct digit * createdigit(int);
-struct digit * append(struct digit *end,struct digit *newptr);
-void print(struct digit *ptr);
+struct digit *creatememory(int);
+struct digit *append(struct digit *, struct digit *);
+struct digit *readnumber();
+void print(struct digit *);
+void freeds(struct digit *);
 int main(void){
-	struct digit * start,* end,*newptr;
-	int number = 1, number2 = 2;
-	newptr = createdigit(number);
-	start = end = newptr;
-	newptr = createdigit(number2);
-	end = append(end,newptr);
+	struct digit * start;
+	start = readnumber();
 	print(start);
+	freeds(start);
+	return 0;
 }
-struct digit * createdigit(int num){
-	struct digit * ptr;
-	ptr = (struct digit *) malloc(sizeof(struct digit));
-	ptr->dig = num;
-	ptr->next = NULL;
-	return ptr;
+struct digit *creatememory(int num){
+	struct digit * newaddy;
+	newaddy = (struct digit *) malloc(sizeof(struct digit));
+	newaddy->n = num;
+	newaddy->next = NULL;
+	return newaddy;
 }
-struct digit * append(struct digit *end,struct digit *newptr){
+struct digit * append(struct digit * end, struct digit * newptr){
 	end->next = newptr;
 	end = newptr;
-	return end;
 }
-void print(struct digit *ptr){
+struct digit * readnumber(){
+	char c;
+	int d;
+	struct digit * start,* newptr, *end;
+	start = NULL;
+	scanf("%c",&c);
+	while(c != '\n'){
+		d = c - 48;
+		newptr = creatememory(d);
+		if(start == NULL){
+			start = end = newptr;
+		}else {
+			end = append(end,newptr);
+		}
+		scanf("%c",&c);
+	}
+	return start;
+}
+void print(struct digit * ptr){
 	struct digit * end;
 	end = ptr;
 	while(end != NULL){
-		printf("%p %d\n",end,end->dig);
-		end = end->next;
+	       printf("(%p : %d)\n",end,end->n);
+	       end = end->next;
 	}
 }
+void freeds(struct digit *start){
+	struct digit * end, *tmp;
+	tmp = end = start;
+	while(end != NULL){
+		tmp = end->next;
+		free(end);
+		end = tmp;
+	}
+}
+
+
